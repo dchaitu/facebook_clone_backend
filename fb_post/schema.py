125a3700fd7ca775cdb274df8b562b6494b7014d
dchaitu <chaitanyadokara@gmail.com>
2025-09-27 22:20:16 +0530

    Added Queries

--- fb_post/schema.py
+++ fb_post/schema.py
@@ -35,12 +35,16 @@ class ReactType(DjangoObjectType):
 
 class Query(ObjectType):
 
-    # all_users = DjangoListField(UserType)
     all_users = graphene.List(UserType)
     all_posts = graphene.List(PostType)
     all_comments = graphene.List(CommentType)
     all_reacts = graphene.List(ReactType)
     all_groups = graphene.List(GroupType)
+    all_posts_by_user = graphene.List(PostType, user_id=graphene.Int(required=True))
+    all_comments_by_post = graphene.List(CommentType, post_id=graphene.Int(required=True))
+    all_reacts_by_post = graphene.List(ReactType, post_id=graphene.Int(required=True))
+    posts_by_user_with_comments_and_reactions = graphene.List(PostType, user_id=graphene.Int(required=True))
+
 
     user = graphene.Field(UserType, user_id=graphene.Int(required=True))
     def resolve_all_users(self, info):
@@ -57,5 +61,17 @@ class Query(ObjectType):
 
     def resolve_all_reacts(self, info):
         return React.objects.all()
+
+    def resolve_all_posts_by_user(self, info, user_id):
+        return Post.objects.filter(posted_by=user_id)
+
+    def resolve_all_comments_by_post(self, info, post_id):
+        return Comment.objects.filter(post=post_id)
+
+    def resolve_all_reacts_by_post(self, info, post_id):
+        return React.objects.filter(post=post_id)
+
+    def resolve_posts_by_user_with_comments_and_reactions(self, info, user_id):
+        return Post.objects.filter(posted_by=user_id).prefetch_related('commenter', 'reacted_to_post')
 
 schema = Schema(query=Query)
