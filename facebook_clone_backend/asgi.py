930c79b0a0c342045d19ce775ac4a48a328d13ea
dchaitu <chaitanyadokara@gmail.com>
2025-09-27 15:06:03 +0530

    Rename project from django_assign006 to facebook_clone_backend

--- facebook_clone_backend/asgi.py
+++ facebook_clone_backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for django_assign006 project.
+ASGI config for facebook_clone_backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_assign006.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "facebook_clone_backend.settings")
 
 application = get_asgi_application()
